165d5b9f30d1c259ff5125c98dcc89cd0506ac6d
Ankush Ladani <astarladani@gmail.com>
2022-06-25 21:39:20 +0530

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Navbar from "./Components/layout/Navbar";
-import {BrowserRouter as Router , Routes , Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./Components/layout/Footer";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import NotFound from "./Pages/NotFound";
 function App() {
   return (
     <Router>
-      {/* <h1>Hello Ankush</h1>
-      <Navbar /> */}
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        {/* p1 -> 0.25rem and 4px */}
+        <Footer />
       </div>
     </Router>
   );
